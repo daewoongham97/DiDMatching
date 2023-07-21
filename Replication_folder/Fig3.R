6abdd677157d4c885b3c23548179b4f47d59d8dc
Dae Woong Ham <74723112+daewoongham97@users.noreply.github.com>
2023-07-21 08:45:00 -0700

    Add files via upload

--- Replication_folder/Fig3.R
+++ Replication_folder/Fig3.R
@@ -1,10 +1,3 @@
-
-#
-# Figure 3 in main paper
-#
-# Plot showing how the three approaches have different biases under different specifications.
-#
-
 library(ggplot2); library(latex2exp); library(gridExtra); library(ggpubr)
 
 # Figure specifications
@@ -56,14 +49,17 @@ for (i in 1:B) {
   DiD_X_Y_bias = DiD_X_Y(b_t_post = b_t_post, b_t_p = b_t_p, b_x_p = b_x_p, delta_theta = delta_theta, b_x_post = b_x_post, delta_x = delta_x[i], sig_theta = sig_theta, sig_x = sig_x, rho = rho, sig_e = sig_e)
   in_df = data.frame(DiM_bias, DiD_bias, DiD_X_bias, DiD_X_Y_bias, reliability)
   df_plot_1 = rbind(df_plot_1, in_df)
-
+  
 }
 
 plot_df_1 = data.frame(bias = abs(c(df_plot_1$DiD_bias, df_plot_1$DiD_X_bias, df_plot_1$DiD_X_Y_bias)), x = rep(delta_x, 3), group = c(rep("Naive DiD", B), rep("Match X", B), rep("Match Both", B)))
 
 plot_df_1$group = factor(plot_df_1$group)
 
-plot_1 = ggplot(data = plot_df_1, aes(x = x, y = bias, col = group)) + geom_line(size = 2) + xlab(TeX("$\\delta_{x}$")) + ylab("Absloute Bias") + ggtitle("")  + theme(axis.text=element_text(size=a1), axis.title=element_text(size=a2,face="bold"), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position=c(0.75, 0.15), legend.title=element_blank(),legend.text=element_text(size=a1), plot.title = element_text(size = a2, face = "bold"), axis.title.x = element_text(vjust=-0.5))
+plot_1 = ggplot(data = plot_df_1, aes(x = x, y = bias, col = group)) + geom_line(size = 2) + 
+  xlab(TeX("$\\delta_{x}$")) + ylab("Absloute Bias") + ggtitle("")  + 
+  theme(axis.text=element_text(size=a1), axis.title=element_text(size=a2,face="bold"), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position=c(0.75, 0.15), legend.title=element_blank(),legend.text=element_text(size=a1), plot.title = element_text(size = a2, face = "bold"), axis.title.x = element_text(vjust=-0.5)) + 
+  scale_color_manual(values = c("Red", "Orange", "Blue"))
 
 # Second plot: Varying reliability of X
 df_plot_2 = data.frame()
@@ -83,14 +79,15 @@ for (i in 1:B) {
   DiD_X_Y_bias = DiD_X_Y(b_t_post = b_t_post, b_t_p = b_t_p, b_x_p = b_x_p, delta_theta = delta_theta, b_x_post = b_x_post, delta_x = delta_x, sig_theta = sig_theta, sig_x = sig_x, rho = rho, sig_e = sig_e)
   in_df = data.frame(DiM_bias, DiD_bias, DiD_X_bias, DiD_X_Y_bias, reliability)
   df_plot_2 = rbind(df_plot_2, in_df)
-
+  
 }
 
 plot_df_2 = data.frame(bias = abs(c(df_plot_2$DiD_bias, df_plot_2$DiD_X_bias, df_plot_2$DiD_X_Y_bias)), x = rep(df_plot_2$reliability, 3), group = c(rep("Naive DiD", B), rep("Match X", B), rep("Match Both", B)))
 
 plot_df_2$group = factor(plot_df_2$group)
 
-plot_2 = ggplot(data = plot_df_2, aes(x = x, y = bias, col = group)) + geom_line(size = 2) + xlab("Reliability") + ylab("") + xlim(c(0,1)) + ggtitle("") + theme(axis.text=element_text(size=a1), axis.title=element_text(size=a2,face="bold"), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position=c(0.75, 0.15), legend.title=element_blank(),legend.text=element_text(size=a1), plot.title = element_text(size = a2, face = "bold"), axis.title.x = element_text(vjust=-0.5))
+plot_2 = ggplot(data = plot_df_2, aes(x = x, y = bias, col = group)) + geom_line(size = 2) + xlab("Reliability") + ylab("") + xlim(c(0,1)) + ggtitle("") + theme(axis.text=element_text(size=a1), axis.title=element_text(size=a2,face="bold"), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position=c(0.75, 0.15), legend.title=element_blank(),legend.text=element_text(size=a1), plot.title = element_text(size = a2, face = "bold"), axis.title.x = element_text(vjust=-0.5)) + 
+  scale_color_manual(values = c("Red", "Orange", "Blue"))
 
 
 # Third plot: Varying correlation rho
@@ -112,26 +109,18 @@ for (i in 1:B) {
   DiD_X_Y_bias = DiD_X_Y(b_t_post = b_t_post, b_t_p = b_t_p, b_x_p = b_x_p, delta_theta = delta_theta, b_x_post = b_x_post, delta_x = delta_x, sig_theta = sig_theta, sig_x = sig_x, rho = rho, sig_e = sig_e)
   in_df = data.frame(DiM_bias, DiD_bias, DiD_X_bias, DiD_X_Y_bias, reliability)
   df_plot_3 = rbind(df_plot_3, in_df)
-
+  
 }
 
 plot_df_3 = data.frame(bias = abs(c(df_plot_3$DiD_bias, df_plot_3$DiD_X_bias, df_plot_3$DiD_X_Y_bias)), x = rep(rhos, 3), group = c(rep("Naive DiD", B), rep("Match X", B), rep("Match Both", B)))
 
 plot_df_3$group = factor(plot_df_3$group)
 
-plot_3 = ggplot(data = plot_df_3, aes(x = x, y = bias, col = group)) + geom_line(size = 2) + xlab("Correlation") + ylab("") + ggtitle("") + theme(axis.text=element_text(size=a1), axis.title=element_text(size=a2,face="bold"), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position=c(0.75, 0.15), legend.title=element_blank(),legend.text=element_text(size=a1), plot.title = element_text(size = a2, face = "bold"), axis.title.x = element_text(vjust=-0.5))
+plot_3 = ggplot(data = plot_df_3, aes(x = x, y = bias, col = group)) + geom_line(size = 2) + xlab("Correlation") + ylab("") + ggtitle("") + theme(axis.text=element_text(size=a1), axis.title=element_text(size=a2,face="bold"), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position=c(0.75, 0.15), legend.title=element_blank(),legend.text=element_text(size=a1), plot.title = element_text(size = a2, face = "bold"), axis.title.x = element_text(vjust=-0.5)) + 
+  scale_color_manual(values = c("Red", "Orange", "Blue"))
 
 fig_3 = ggarrange(plot_1, plot_2, plot_3, common.legend = TRUE, nrow = 1)
 
-fig_3
-
-ggsave(file="Figures/Fig3.pdf", fig_3,  width = 12, height = 5, device = "pdf")
-
-
-
-
-
-
-
+ggsave(file="/Fig3.pdf", fig_3,  width = 12, height = 5, device = "pdf")
 
 
